af27105deff635b75e2957789355d3d7e98e8d90
aminsoheyli <aminsoheyli98@gmail.com>
2026-06-22 19:52:37 +0330

    add server app

---
+++ quickstart/server/app.py
@@ -0,0 +1,44 @@
+from concurrent import futures
+
+import grpc
+
+import users_pb2
+import users_pb2_grpc
+
+
+class Users(users_pb2_grpc.UsersServicer):
+    def GetUsers(self, request, context):
+        return users_pb2.GetUsersResponse(users=[
+            users_pb2.User(
+                id='1',
+                name='test 1',
+                email='test1@gmail.com',
+                password='test1@123'
+            ),
+            users_pb2.User(
+                id='2',
+                name='test 2',
+                email='test2@gmail.com',
+                password='test2@123'
+            ),
+            users_pb2.User(
+                id='3',
+                name='test 3',
+                email='test3@gmail.com',
+                password='test3@123'
+            )
+        ])
+
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    users_pb2_grpc.add_UsersServicer_to_server(Users(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
