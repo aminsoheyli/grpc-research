201fcc1889b12a95133e6bfec0b9cd7dcd91e958
aminsoheyli <aminsoheyli98@gmail.com>
2026-06-22 19:55:23 +0330

    add client app

---
+++ quickstart/client/app.py
@@ -0,0 +1,15 @@
+import grpc
+
+import users_pb2
+import users_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = users_pb2_grpc.UsersStub(channel)
+        response = stub.GetUsers(users_pb2.GetUsersRequest())
+    print(response.users[0])
+
+
+if __name__ == "__main__":
+    run()
