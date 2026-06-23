1cf378c972f697c159068c42ac36f8407b434632
aminsoheyli <aminsoheyli98@gmail.com>
2026-06-23 16:00:44 +0330

    add createTodo to server

--- todo-app/server/app.py
+++ todo-app/server/app.py
@@ -1,6 +1,57 @@
-def main():
-    print("Hello from todo-server!")
+import logging
+import sys
+from concurrent import futures
+
+import grpc
+
+import todo_pb2
+import todo_pb2_grpc
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    stream=sys.stdout,
+    force=True
+)
+
+todos = [
+    todo_pb2.TodoItem(
+        id=1,
+        text='todo 1',
+    ),
+    todo_pb2.TodoItem(
+        id=1,
+        text='todo 1',
+    ),
+    todo_pb2.TodoItem(
+        id=1,
+        text='todo 1',
+    )
+]
+
+logger = logging.getLogger(__name__)
+
+
+class Todos(todo_pb2_grpc.TodoServicer):
+    def createTodo(self, request, context):
+        new_todo = todo_pb2.TodoItem(
+            id=len(todos) + 1,
+            text=request.text
+        )
+        todos.append(new_todo)
+
+        return new_todo
+
+
+def serve():
+    port = "8080"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    todo_pb2_grpc.add_TodoServicer_to_server(Todos(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    logging.info("Server started, listening on " + port)
+    server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    main()
+    serve()
